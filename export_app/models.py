e6fc5564aef8952bb4a6b837acf550bcf9405e77
vesloguzov <vesloguzov@gmail.com>
2018-08-11 20:24:32 +0500

    projects list #52

--- export_app/models.py
+++ export_app/models.py
@@ -26,7 +26,8 @@ class TildaRequest(models.Model):
         print("STEP 1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         if self.request_count < 120:
             # request = requests.get(f'{self.base_url}getprojectslist/?publickey={self.publickey}&secretkey={self.secretkey}')
-            request = requests.get(f"{self.base_url}getprojectslist/?publickey={self.publickey}&secretkey={self.secretkey}")
+            request = requests.get(
+                "{}getprojectslist/?publickey={}&secretkey={}".format(self.base_url, self.publickey, self.secretkey))
             self.increment()
             response = request.json()
             print(response)
