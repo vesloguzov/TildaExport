b60c18fba23c187fbc6fd87b51c370fcb8f3a309
vesloguzov <vesloguzov@gmail.com>
2018-08-13 13:40:37 +0500

    page page #8

--- tildaexport/urls.py
+++ tildaexport/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('projects/', include('export_app.urls')),
 ]
 
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
