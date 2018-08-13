0f7a54c22e34219985a04658a4b3e68b6abf790d
vesloguzov <vesloguzov@gmail.com>
2018-08-13 10:35:37 +0500

    project page#6

--- export_app/urls.py
+++ export_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.projects_list, name='projects_list'),
     path('update_projects/', views.update_projects, name='update_projects'),
     path('<int:project_id>/', views.project, name='project'),
-    path('<int:project_id>/update_project', views.update_project, name='update_project'),
+    path('<int:project_id>/update_project/', views.update_project, name='update_project'),
 ]
