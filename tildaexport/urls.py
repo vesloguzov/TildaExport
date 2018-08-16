da721512dd9b97b7543a0d1613a0043dc702ce0c
MasterGowen <mastergowen@gmail.com>
2018-08-16 17:20:13 +0500

    static#1

--- tildaexport/urls.py
+++ tildaexport/urls.py
@@ -4,10 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
 from export_app import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('export_app.urls')),
+    path('projects/', include('export_app.urls')),
+    path('', RedirectView.as_view(url='projects', permanent=False), name='index')
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
