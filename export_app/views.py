c349858e3ed123ccec81bc434c342f52c2ab0c6b
Vesloguzov Dmitry <vesloguzov@gmail.com>
2018-08-18 23:29:02 +0500

    hkj,dvdf

--- export_app/views.py
+++ export_app/views.py
@@ -51,7 +51,6 @@ def project(request, project_id):
     context = dict()
     context["project"] = project
     context["pages"] = []
-    context["is_project"] = True
     for page in project.ProjectPages.all():
         context["pages"].append(page)
     return render(request, "project.html", context)
