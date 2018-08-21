12698f24493fd90b0a3110234baada159bd898a9
vesloguzov <vesloguzov@gmail.com>
2018-08-21 13:14:49 +0500

    prod#14

--- export_app/models.py
+++ export_app/models.py
@@ -149,7 +149,9 @@ class TildaRequest(models.Model):
                     page_object.filename = page["filename"]
                     if page["html"] is not None:
                         print(page["html"])
-                        page_object.html = page["html"].replace('</head>', '</head><script type="text/javascript">window.onload = function() {parent.iframeLoaded();}; $.getScript("https://cdn.jsdelivr.net/npm/iframe-resizer@3.5.16/js/iframeResizer.contentWindow.js")</script>', 1)
+                        page_object.html = page["html"].replace('</head>',
+                                                                '</head><script type="text/javascript">window.onload = function() {parent.iframeLoaded();}; $.getScript("https://cdn.jsdelivr.net/npm/iframe-resizer@3.5.16/js/iframeResizer.contentWindow.js")</script>',
+                                                                1)
                     else:
                         page_object.html = ""
                     page_object.last_updated = datetime.now()
@@ -207,7 +209,9 @@ class Page(models.Model):
     def save_iframe_code(self):
         print(self.filename.split('.')[0])
         self.iframe = '<script type="text/javascript">$.getScript("https://cdn.jsdelivr.net/npm/iframe-resizer@3.6.1/src/iframeResizer.min.js")</script><iframe src="{}" onload="{}" id="{}" width="100%" frameborder="0" scrolling="no"></iframe>' \
-            .format(get_site_addr() + self.page_path, "function iframeLoaded() {[iFrameResize({}, '#{}')}}".format('{log:true}', self.filename.split('.')[0]), self.filename.split('.')[0])
+            .format(get_site_addr() + self.page_path,
+                    "function iframeLoaded() {[iFrameResize({{log:true}}, '#{}')}}".format(self.filename.split('.')[0]),
+                    self.filename.split('.')[0])
         pass
 
     def save_html_file(self):
